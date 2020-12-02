87f4b31ce5987e48611aedde13c9892c9790e040
voyevoda <elisa9el@gmail.com>
2020-12-02 01:19:57 -0500

    Moved Util dir to root where it was intended to be

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server')
 
+const { validateRegisterInput } = require('../../Util/validators')
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
